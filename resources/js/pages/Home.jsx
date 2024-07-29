0ff3b573cf5a827e34200557da4131f22d378d23
Ghozy Alkhairi <92246826+ghozyalkhairi@users.noreply.github.com>
2024-07-29 10:06:17 +0700

    new up

---
+++ resources/js/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Head } from "@inertiajs/react";
+import Layout from "../components/Layout";
+
+const Home = () => {
+    return (
+        <Layout>
+            <Head title="Welcome" />
+            <h1>Welcome</h1>
+            <p>Hello</p>
+        </Layout>
+    );
+};
+
+export default Home;
